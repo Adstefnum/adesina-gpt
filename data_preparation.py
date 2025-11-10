ff2f2536d7818b2f72c9f78970236811a90cab04
Stephen Adesina <bauerstevenson@gmail.com>
2025-11-10 22:36:49 +0200

    converting words into tokens

--- data_preparation.py
+++ data_preparation.py
@@ -1,6 +1,17 @@
+import re
+
 with open("verdict.txt", "r") as file:
     data = file.read()
     print(f"no of characters: {len(data)}")
     print(f"no of words: {len(data.split())}")
-    print(data[:99])
+    # print(data[:99])
+
+
+    # tokenize with regex, remove space
+    tokens = re.split(r'(\s+)|([,.;:?_()!"\']|--)', data)
+    tokens = [token.strip() for token in tokens if token is not None and token.strip()]
+    print(f"no of tokens: {len(tokens)}")
+    # print(tokens[:30])
+
+# convert into token ids
     
