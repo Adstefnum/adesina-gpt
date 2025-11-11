1faddf654b3630fa5edca081dd0de5506476cc40
Stephen Adesina <bauerstevenson@gmail.com>
2025-11-11 03:05:12 +0200

    adding absolute positional embeddings to keep track of position's influence over text

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import data_loader
+import torch
 
+vocab_size = 50257
+output_dim = 256
+chunk_size = 4
 
 if __name__ == "__main__":
     with open("verdict.txt", "r") as file:
         text = file.read()
 
     dataloader = data_loader.create_data_loader(
-        text, batch_size=8, stride=4, shuffle=False, chunk_size=4)
+        text, batch_size=8, stride=chunk_size, shuffle=False, chunk_size=chunk_size)
     data_iter = iter(dataloader)
-    first_batch = next(data_iter)
-    print(first_batch)
+    inputs, targets = next(data_iter)
+
+    torch.manual_seed(0)
+    token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+    token_embeddings = token_embedding_layer(inputs)
+    print(token_embeddings.shape)
+
+    context_length = chunk_size
+    pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
+    pos_embeddings = pos_embedding_layer(torch.arange(context_length))
+    print(pos_embeddings.shape)
+
+    input_embeddings = token_embeddings + pos_embeddings
+    print(input_embeddings.shape)
