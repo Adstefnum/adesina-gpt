d558a896b8cdb7a3dfe13260f4a9cfdd77e629a9
Stephen Adesina <bauerstevenson@gmail.com>
2025-11-11 17:55:55 +0200

    using linear instead of parameter as linear has stable parameter init that also makes training more stable than manually doing it with parameter

--- attention.py
+++ attention.py
@@ -1,18 +1,18 @@
 import torch
 
 class SelfAttention(torch.nn.Module):
-    def __init__(self, input_dim, output_dim):
+    def __init__(self, input_dim, output_dim, qkv_bias=False):
         super().__init__()
-        self.W_query = torch.nn.Parameter(torch.rand(input_dim, output_dim), requires_grad=False)
-        self.W_key = torch.nn.Parameter(torch.rand(input_dim, output_dim), requires_grad=False)
-        self.W_value = torch.nn.Parameter(torch.rand(input_dim, output_dim), requires_grad=False)
+        self.W_query = torch.nn.Linear(input_dim, output_dim, bias=qkv_bias)
+        self.W_key = torch.nn.Linear(input_dim, output_dim, bias=qkv_bias)
+        self.W_value = torch.nn.Linear(input_dim, output_dim, bias=qkv_bias)
         
     def forward(self, inputs):
-        queries = inputs @ self.W_query
-        keys = inputs @ self.W_key
-        values = inputs @ self.W_value
+        queries = self.W_query(inputs)
+        keys = self.W_key(inputs)
+        values = self.W_value(inputs)
         
-        attention_scores = queries @ keys.transpose(-2, -1)
+        attention_scores = queries @ keys.transpose(-2,-1)
         attention_weights = torch.softmax(attention_scores / torch.sqrt(torch.tensor(keys.shape[-1])), dim=-1)
         context_vector = attention_weights @ values
         
