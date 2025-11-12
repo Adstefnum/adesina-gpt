88c8b19c527026340aaef25ffbb856c746f7cb73
Stephen Adesina <bauerstevenson@gmail.com>
2025-11-12 23:43:20 +0200

    shipping proper multi head attenton not just concatentaing multiple causal heads. Starts out by getting shapes, turning qkv into linear representations, then reshaping them into a 4d thing, then transposing first and second dims so as not to mix the computation of tokens by the ehads, then calculate attention scores, mask it, get the attn wieghts, apply dropout, transpose the 1 adn 2 dim back since token comp by head is  done, then make sure all tokens are stored in contiguous memory(stored together) and reshape again to proper dims(3 not 4. we're concatentating all results from the various attention heads), then push out a final linear transformation to best combine all of them. still a bit hazy in my head

--- attention.py
+++ attention.py
@@ -39,7 +39,41 @@ class CausalAttention(SelfAttention):
 class MultiHeadAttention(torch.nn.Module):
     def __init__(self, input_dim, output_dim, context_length, dropout, num_heads, qkv_bias=False):
         super().__init__()
-        self.heads = torch.nn.ModuleList([CausalAttention(input_dim, output_dim, context_length, dropout, qkv_bias) for _ in range(num_heads)])
+        assert (output_dim % num_heads == 0), "Output dimension must be divisible by number of heads"
+        self.head_dim = output_dim // num_heads
+        self.num_heads = num_heads
+        self.output_dim = output_dim
+        self.W_query = torch.nn.Linear(input_dim, output_dim, bias=qkv_bias)
+        self.W_key = torch.nn.Linear(input_dim, output_dim, bias=qkv_bias)
+        self.W_value = torch.nn.Linear(input_dim, output_dim, bias=qkv_bias)
+        self.dropout = torch.nn.Dropout(dropout)
+        self.context_length = context_length
+        self.out_proj = torch.nn.Linear(output_dim, output_dim)
+        self.register_buffer("causal_mask", torch.triu(torch.ones(context_length, context_length), diagonal=1))
 
     def forward(self, inputs):
-        return torch.cat([head(inputs) for head in self.heads], dim=-1)
+        batch_size, num_tokens, input_dim = inputs.shape
+
+        keys = self.W_key(inputs)
+        values = self.W_value(inputs)
+        queries = self.W_query(inputs)
+        
+        keys = keys.view(batch_size, num_tokens, self.num_heads, self.head_dim)
+        values = values.view(batch_size, num_tokens, self.num_heads, self.head_dim)
+        queries = queries.view(batch_size, num_tokens, self.num_heads, self.head_dim)
+
+        keys = keys.transpose(1,2)
+        values = values.transpose(1,2)
+        queries = queries.transpose(1,2)
+
+        attention_scores = queries @ keys.transpose(-2,-1)
+        masked_attention_scores = attention_scores.masked_fill(self.causal_mask.bool(), -torch.inf)
+        attention_weights = torch.softmax(masked_attention_scores / torch.sqrt(torch.tensor(keys.shape[-1])), dim=-1)
+        context_vector = (self.dropout(attention_weights) @ values).transpose(1,2)
+        
+        context_vector = context_vector.contiguous().view(batch_size, num_tokens, self.output_dim)
+        context_vector = self.out_proj(context_vector)
+        
+        return context_vector
+        
+
