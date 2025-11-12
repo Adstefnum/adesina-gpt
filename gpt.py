c7f22e36da258b2ccb1eb8bef1a7362ea66ef001
Stephen Adesina <bauerstevenson@gmail.com>
2025-11-13 01:26:11 +0200

    adding the skeleton of the gpt arch that we will bake in a bunch of components into the transformer which will build up the gpt model so we can have multiple gpt blocks for training.

---
+++ gpt.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+
+
+class GPT(nn.Module):
+    def __init__(self, input_dim, output_dim, context_length, dropout, num_heads, qkv_bias=False):
+        pass
+
+    def forward(self, inputs):
+        pass
+        
+
+class TransformerBlock(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, inputs):
+        pass
+
+class LayerNorm(nn.Module):
+    def __init__(self, input_dim):
+        pass
+
+    def forward(self, inputs):
+        pass
