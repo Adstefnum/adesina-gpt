ffe21ec62bbda768d80c91433ab35810219166c2
Stephen Adesina <bauerstevenson@gmail.com>
2025-11-17 17:08:06 +0200

    changing the batch size and printing out some stats

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         text = file.read()
 
     dataloader = data_loader.create_data_loader(
-        text, batch_size=8, stride=chunk_size, shuffle=False, chunk_size=chunk_size)
+        text, batch_size=2, stride=chunk_size, shuffle=False, chunk_size=chunk_size)
     data_iter = iter(dataloader)
     inputs, targets = next(data_iter)
 
@@ -19,6 +19,13 @@ if __name__ == "__main__":
     model = GPT(config)
     logits = model(inputs)
     print(logits.shape)  
+
+    total_params = sum(p.numel() for p in model.parameters())
+    print(f"Total parameters: {total_params:,}")
+
+    total_bytes = total_params * 4
+    print(f"Total bytes: {total_bytes:,}")
+    print(f"Total MB: {total_bytes / 1024 / 1024}")
 
     # attention = MultiHeadAttention(output_dim, output_dim, context_length, dropout=0.0, num_heads=8)
     # context_vectors = attention.forward(input_embeddings)
