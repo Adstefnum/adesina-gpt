450607de1b455c4008d3185771b0f060dec16ac0
Stephen Adesina <bauerstevenson@gmail.com>
2025-11-21 22:43:31 +0200

    calculating and testing loss by using cross entropy Which works by getting the logits by passing the input I/Ds to the model. Then you get the probabilities for the input and then from that probs for the target I/Ds. Then you convert them to logarithms because they are easier to work with. Then you get the average. Then you get the negative probability, negative of the average, which is the cross entropy. Or the negative average logarithmic probability.

--- utils.py
+++ utils.py
@@ -20,4 +20,33 @@ def text_to_tokens(tokenizer, text):
 def tokens_to_text(token_ids, tokenizer):
     tokens = token_ids.squeeze(0)
     return tokenizer.decode(tokens.tolist())
+
+def calc_batch_loss(input_ids, target_ids, model, device):
+    input_ids = input_ids.to(device)
+    target_ids = target_ids.to(device)
+    logits = model(input_ids)
+    loss = torch.nn.functional.cross_entropy(logits.flatten(0,1), target_ids.flatten())
+    return loss
+
+def calc_loss_loader(dataloader, model, device, num_batches=None):
+    total_loss = 0
+    if len(dataloader) == 0:
+        return float('nan')
+
+    elif num_batches is None:
+        num_batches = len(dataloader)
+
+    else:
+        num_batches = min(num_batches, len(dataloader))
+
+    for i, (input_ids, target_ids) in enumerate(dataloader):
+        loss = calc_batch_loss(input_ids, target_ids, model, device)
+        if i < num_batches:
+            total_loss += loss.item()
+        else:
+            break
+    return total_loss/ num_batches
+
+    
+    
             
